bfd9050b3306dab16f129036dd7f0eba28f98b9b
zhuxiangyuu <zhuxiangyuu@qq.com>
2018-12-12 09:08:18 +0800

    first commit

---
+++ src/main/java/com/bdqn/oa/TestGitApplication.java
@@ -0,0 +1,12 @@
+package com.bdqn.oa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGitApplication.class, args);
+    }
+}
